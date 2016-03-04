af18372e96c87e8148b16cb003f7edf62a60af92
Minjoon Seo <seominjoon@gmail.com>
2016-03-04 01:50:11 -0800

    fixed bug

--- shining3/split_dqa.py
+++ shining3/split_dqa.py
@@ -12,20 +12,24 @@ def get_args():
     parser.add_argument("first_dir")
     parser.add_argument("--second_dir", default="", type=str)
     parser.add_argument('--num', default=0, type=int)
+    parser.add_argument('--start', type=int, default=0)
+    parser.add_argument('--stop', type=int, default=1500)
     parser.add_argument('--skip_images', type=str, default='False')
     parser.add_argument('--random', type=str, default='True')
     parser.add_argument('--label', type=str, default='False', help='label=False ignores questions on labeled images')
+    parser.add_argument('--quiet', type=str, default='True')
 
     return parser.parse_args()
 
 
-def _copy(from_dir, to_dir, name, subdir=""):
+def _copy(from_dir, to_dir, name, subdir="", quiet=True):
     from_path = os.path.join(from_dir, subdir, name)
     to_path = os.path.join(to_dir, subdir, name)
     if os.path.exists(from_path):
         shutil.copy(from_path, to_path)
     else:
-        logging.warning("%s does not exist." % from_path)
+        if not quiet:
+            logging.warning("%s does not exist." % from_path)
 
 
 def split_dqa(args):
@@ -37,7 +41,8 @@ def split_dqa(args):
     if second_dir and not os.path.exists(second_dir):
         os.mkdir(second_dir)
     num = args.num
-    image_names = [name for name in os.listdir(os.path.join(data_dir, "images")) if name.endswith(".png")]
+    image_names = [name for name in os.listdir(os.path.join(data_dir, "images"))
+                   if name.endswith(".png") and args.start <= int(os.path.splitext(name)[0]) < args.stop]
     if args.random == 'True':
         random.shuffle(image_names)
     if num:
@@ -68,15 +73,17 @@ def split_dqa(args):
                     _copy(data_dir, to_dir, image_name, subdir=subdir)
             _copy(data_dir, to_dir, json_name, subdir='annotations')
 
-            if args.label:
+            if args.label == 'True':
                 _copy(data_dir, to_dir, json_name, subdir='questions')
             else:
-                question_json = json.load(open(os.path.join(data_dir, 'questions', json_name), 'rb'))
-                keys = question_json['questions'].keys()
-                for key in keys:
-                    if question_json['questions'][key]['abcLabel']:
-                        del question_json['questions'][key]
-                json.dump(question_json, open(os.path.join(to_dir, 'questions', json_name), 'wb'))
+                question_path = os.path.join(data_dir, 'questions', json_name)
+                if os.path.exists(question_path):
+                    question_json = json.load(open(question_path, 'rb'))
+                    keys = question_json['questions'].keys()
+                    for key in keys:
+                        if question_json['questions'][key]['abcLabel']:
+                            del question_json['questions'][key]
+                    json.dump(question_json, open(os.path.join(to_dir, 'questions', json_name), 'wb'))
             pbar.update(i)
         pbar.finish()
     else:
